a8f230f950cd4657ce310d9eec76fe90085803b2
skarn <118652388+skroud@users.noreply.github.com>
2022-12-22 21:57:04 -0600

    Update ram.py

--- core/utilities/ram.py
+++ core/utilities/ram.py
@@ -1,5 +1,5 @@
-import sys, os, time, subprocess
+import sys, os, time, psutil
 
 class ram():
-    ram=subprocess.getoutput(r" free -m | awk 'NR==2{printf  $3*100/$2 }'  | cut -c 1-5")
-    print(f"{ram}%")
+    ram=psutil.virtual_memory().percent
+    print(f"{ram}")
