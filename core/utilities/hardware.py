33557282a6aa0e381adee003a4a210a9b379016f
skarn <118652388+skroud@users.noreply.github.com>
2022-12-24 16:08:09 -0600

    finished hardware [show cpu_count, cpu_freq, cpu_percent, mem_info, mem_total, mem_used, mem_free, disk_info, disk_total, disk_used, disk_free

---
+++ core/utilities/hardware.py
@@ -0,0 +1,30 @@
+import psutil
+
+class hardware():
+    def show_hrwr_info():
+        cpu_count = psutil.cpu_count()
+        cpu_freq = psutil.cpu_freq()
+        cpu_percent = psutil.cpu_percent()
+
+        print(f'CPU Count: {cpu_count}')
+        print(f'CPU Frequency: {cpu_freq.current:.0f} MHz')
+        print(f'CPU Utilization: {cpu_percent}%')
+
+        mem_info = psutil.virtual_memory()
+        mem_total = mem_info.total / (1024 ** 2)
+        mem_used = mem_info.used / (1024 ** 2)
+        mem_free = mem_info.free / (1024 ** 2)
+
+        print(f'Total Memory: {mem_total:.0f} MB')
+        print(f'Used Memory: {mem_used:.0f} MB')
+        print(f'Free Memory: {mem_free:.0f} MB')
+
+        disk_info = psutil.disk_usage('/')
+        disk_total = disk_info.total / (1024 ** 3)
+        disk_used = disk_info.used / (1024 ** 3)
+        disk_free = disk_info.free / (1024 ** 3)
+
+        print(f'Total Disk Space: {disk_total:.0f} GB')
+        print(f'Used Disk Space: {disk_used:.0f} GB')
+        print(f'Free Disk Space: {disk_free:.0f} GB')
+    show_hrwr_info()
