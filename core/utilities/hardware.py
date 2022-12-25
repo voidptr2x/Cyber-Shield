956ca1ae5c136d198e116bfe8d3a5c4b72c58361
Nefarious <c0deChieff@gmail.com>
2022-12-25 01:20:20 -0500

    Update hardware.py

--- core/utilities/hardware.py
+++ core/utilities/hardware.py
@@ -1,14 +1,11 @@
 import psutil
-
-class hardware():
-    def __setinfo(self) -> HardwareInformation:
         
 class HardwareInformation():
     """
     Hardware
     """
     cpu_count: int
-    cpu: str
+    cpu_name: str
     cpu_cores: str
     cpu_freq: str
     cpu_usage: str
@@ -16,6 +13,7 @@ class HardwareInformation():
     """
     Memory Information
     """
+    memory_name: str
     memory_capacity: str
     memory_used: str
     memory_free: str
@@ -23,7 +21,42 @@ class HardwareInformation():
     """
     Hard drive Information
     """
-    hdd: str
+    hdd_name: str
     hdd_capacity: str
     hdd_used: set
     hdd_free: str
+
+
+"""
+    Use Example:
+    
+    hdw = Hardware()
+    hdw.info.OBJECTS_HERE
+"""
+class Hardware():
+    info: HardwareInformation
+    def __init__(self) -> None:
+        self.__fetchCPU()
+        self.__fetchMemory()
+        self.__fetchHDD()
+
+    def __fetchCPU(self) -> None:
+        self.info.cpu_count = psutil.cpu_count()
+        self.info.cpu_name = "THIS IS NEEDED"
+        self.info.cpu_freq = psutil.cpu_freq()
+        self.info.cpu_usage = psutil.cpu_percent()
+
+    def __fetchMemory(self) -> None:
+        mem_info = psutil.virtual_memory()
+        self.info.memory_name = "THIS IS NEEDED"
+        self.info.memory_capacity = mem_info.total / (1024 ** 2)
+        self.info.memory_used = mem_info.used / (1024 ** 2)
+        self.info.memory_free = mem_info.free / (1024 ** 2)
+
+    
+    def __fetchHDD(self) -> None:
+        disk_info = psutil.disk_usage('/')
+        self.info.hdd_name = "THIS IS NEEDED"
+        self.info.hdd_capacity = disk_info.total / (1024 ** 3)
+        self.info.hdd_usage = disk_info.used / (1024 ** 3)
+        self.info.hdd_free = disk_info.free / (1024 ** 3)
