fe19da28c4953a94a78d4522ed1d3734836c0cb8
syyn <94141577+9xN@users.noreply.github.com>
2022-12-31 13:52:38 -0700

    Add functions to cover more of assets/config.json

--- core/utilities/config/config.py
+++ core/utilities/config/config.py
@@ -39,5 +39,39 @@ class Config():
                 self.conntable.border_c = self.config['Conn_Table']['border_c']
                 self.conntable.header_text_c = self.config['Conn_Table']['header_text_c']
 
+        def retrieveOS(self) -> os:
+                self.os.labels_c = self.config['OS_Display']['labels_c']
+                self.os.value_c = self.config['OS_Display']['value_c']
+                self.os.os_name_p = self.config['OS_Display']['os_name_p']
+                self.os.os_kernel_p = self.config['OS_Display']['os_kernel_p']
+                self.os.os_version_p = self.config['OS_Display']['os_version_p']
+                self.os.shell_p = self.config['OS_Display']['shell_p']
+
         def retrieveHardware(self) -> hdw:
-                pass
+                self.hdw.labels_c = self.config['Hardware']['labels_c']
+                self.hdw.value_c = self.config['Hardware']['value_c']
+                self.hdw.cpu_count_p = self.config['Hardware']['cpu_count_p']
+                self.hdw.cpu_name_p = self.config['Hardware']['cpu_name_p']
+                self.hdw.cpu_cores_p = self.config['Hardware']['cpu_cores_p']
+                self.hdw.cpu_usage_p = self.config['Hardware']['cpu_usage_p']
+                self.hdw.gpu_name_p = self.config['Hardware']['gpu_name_p']
+                self.hdw.gpu_cores_p = self.config['Hardware']['gpu_cores_p']
+                self.hdw.gpu_usage_p = self.config['Hardware']['gpu_usage_p']
+                self.hdw.memory_name_p = self.config['Hardware']['memory_name_p']
+                self.hdw.memory_capacity_p = self.config['Hardware']['memory_capacity_p']
+                self.hdw.memory_usage_p = self.config['Hardware']['memory_usage_p']
+                self.hdw.memory_used_p = self.config['Hardware']['memory_used_p']
+                self.hdw.memory_free_p = self.config['Hardware']['memory_free_p']
+                self.hdw.hdd_name_p = self.config['Hardware']['hdd_name_p']
+                self.hdw.hdd_capacity_p = self.config['Hardware']['hdd_capacity_p']
+                self.hdw.hdd_used_p = self.config['Hardware']['hdd_used_p']
+                self.hdw.hdd_free_p = self.config['Hardware']['hdd_free_p']
+                self.hdw.hdd_usage_p = self.config['Hardware']['hdd_usage_p']
+        
+        def retrieveConnection(self) -> conn:
+                self.conn.labels_c = self.config['Connection']['labels_c']
+                self.conn.value_c = self.config['Connection']['value_c']
+                self.conn.ip = self.config['Connection']['ip']
+                self.conn.port = self.config['Connection']['port']
+                self.conn.key = self.config['Connection']['key']
+                self.conn.timeout = self.config['Connection']['timeout']
