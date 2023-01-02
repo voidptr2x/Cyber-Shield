aec3b3362df4a2f31e32f2a0984647700cd1a223
Nefarious <c0deChieff@gmail.com>
2023-01-02 06:46:36 -0500

    updates

--- core/monitor.py
+++ core/monitor.py
@@ -10,10 +10,6 @@ class CyberShield():
     interfaces: list
     
     def __init__(self) -> None:
-        # grab config information
-        # display os, hardware, connection, motd and other information that doesn't update
-
-        # LATER THING: start socket server here
         self.sfx = ShieldFX()
         self.cfg = Config()
         self.os = OS()
@@ -23,14 +19,33 @@ class CyberShield():
         print("\x1b[0;0f")
         print(self.sfx.render_ui(), end="") # Set UI
 
+        self.set_info()
         self.start_listener()
 
-    def start_listener(self) -> dNone:
-        self.pps = PPS("eth0")
-
+    def set_info(self) -> None:
         if self.cfg.os.display == True:
-            print(f"\x1b[{self.cfg.os.os_name_p[0]};{self.cfg.os.os_name_p[1]}f", end=" ")
-            print(f"OS: {self.os.os_name}")
+            if self.cfg.os.os_name_p != [0, 0]:
+                print(f"\x1b[{self.cfg.os.os_name_p[0]};{self.cfg.os.os_name_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
+                print(f"OS: ", end="")
+                print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
+                print(self.os.os_name, end="")
+
+                print(f"\x1b[{self.cfg.os.os_version_p[0]};{self.cfg.os.os_version_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
+                print(f"OS Version: ", end="")
+                print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
+                print(self.os.os_version, end="")
+
+
+                print(f"\x1b[{self.cfg.os.os_kernel_p[0]};{self.cfg.os.os_kernel_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
+                print(f"OS Kernel: ", end="")
+                print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
+                print(self.os.os_kernel, end="")
+
+
+                print("\x1b[0m", end="")
+
+    def start_listener(self) -> None:
+        self.pps = PPS("eth0")
 
         while True:
             print(f"\x1b[{self.cfg.ppscfg.pps_p[0]};{self.cfg.ppscfg.pps_p[1]}f", end=" ")
