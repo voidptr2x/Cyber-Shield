68c63229100be01a32111fbec0810393a95e5072
Nefarious <c0deChieff@gmail.com>
2023-01-04 11:48:17 -0500

    print clean up unfinished

--- core/monitor.py
+++ core/monitor.py
@@ -33,29 +33,13 @@ class CyberShield():
         if self.cfg.os.display == True:
             if self.cfg.os.os_name_p != [0, 0]:
                 TerminalControl.placeText(self.cfg.os.os_name_p, self.cfg.os.labels_c, "OS: ", self.cfg.os.value_c, self.os.info.os_name)
-                # print(f"\x1b[{self.cfg.os.os_name_p[0]};{self.cfg.os.os_name_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
-                # print(f"OS: ", end="")
-                # print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
-                # print(self.os.info.os_name, end="")
-
-                print(f"\x1b[{self.cfg.os.os_version_p[0]};{self.cfg.os.os_version_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
-                print(f"OS Version: ", end="")
-                print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
-                print(self.os.info.os_version, end="")
-
-
-                print(f"\x1b[{self.cfg.os.os_kernel_p[0]};{self.cfg.os.os_kernel_p[1]}f\x1b[38;2;{self.cfg.os.labels_c[0]};{self.cfg.os.labels_c[1]};{self.cfg.os.labels_c[2]}m", end="")
-                print(f"OS Kernel: ", end="")
-                print(f"\x1b[38;2;{self.cfg.os.value_c[0]};{self.cfg.os.value_c[1]};{self.cfg.os.value_c[2]}m", end="")
-                print(self.os.info.os_kernel, end="")
+                TerminalControl.placeText(self.cfg.os.os_version_p, self.cfg.os.labels_c, "OS Version: ", self.cfg.os.value_c, self.os.info.os_version)
+                TerminalControl.placeText(self.cfg.os.os_kernel_p, self.cfg.os.labels_c, "OS Kernel: ", self.cfg.os.value_c, self.os.info.os_kernel)
 
         if self.cfg.hdw.display == True:
             if self.cfg.hdw.cpu_name_p != [0, 0]:
-                print(f"\x1b[{self.cfg.hdw.cpu_name_p[0]};{self.cfg.hdw.cpu_name_p[1]}f\x1b[38;2;{self.cfg.hdw.labels_c[0]};{self.cfg.hdw.labels_c[1]};{self.cfg.hdw.labels_c[2]}m", end="")
-                print(f"CPU Name: ", end="")
-                print(f"\x1b[38;2;{self.cfg.hdw.value_c[0]};{self.cfg.hdw.value_c[1]};{self.cfg.hdw.value_c[2]}m", end="")
-                print(self.hdw.info.cpu_name, end="")
-                print("\x1b[0m", end="")
+                TerminalControl.placeText(self.cfg.hdw.cpu_name_p, self.cfg.hdw.labels_c, "CPU Name: ", self.cfg.hdw_value_c, self.hdw.info.cpu_name)
+                
             # if self.cfg.hdw.cpu_cores_p != [0, 0]:
             #     print(f"\x1b[{self.cfg.hdw.cpu_cores_p[0]};{self.cfg.hdw.cpu_cores_p[1]}f\x1b[38;2;{self.cfg.hdw.labels_c[0]};{self.cfg.hdw.labels_c[1]};{self.cfg.hdw.labels_c[2]}m", end="")
             #     print(f"CPU Cores: ", end="")
