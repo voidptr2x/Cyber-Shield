88ead698a7d849dcd6752beca7b78d7ac0fba90e
rgwitnp <121322199+rgwitnp@users.noreply.github.com>
2023-01-05 18:30:08 -0800

    Update connection.py
    added new get_interface_statistics function

--- core/tools/connection.py
+++ core/tools/connection.py
@@ -19,3 +19,56 @@ class Connection():
             self.f_pps = 0
             self.f_pps = (tx - new_tx) - (rx - new_rx)
             print(self.f_pps, end="\r")
+            
+    """
+    Run this to get all of the network statistics for all interfaces
+    """
+    def get_interface_statistics(self):
+        """
+        Returns a dictionary of the statistics for all network interfaces.
+
+        The first key is the interface name, the second key is the statistics for that interface.
+
+        The statistics are named as follows
+        'bytesIn',  'packetsIn',  'errorsIn',  'dropsIn',  'fifoIn',  'frameIn',  'compressedIn',  'multicastIn',
+        'bytesOut', 'packetsOut', 'errorsOut', 'dropsOut', 'fifoOut', 'frameOut', 'compressedOut', 'multicastOut'
+
+        Example returned dictionary
+        {
+        'enp0s25': 
+            {'bytesIn': '0', 'packetsIn': '0', 'errorsIn': '0', 'dropsIn': '0', 'fifoIn': '0', 'frameIn': '0',
+            'compressedIn': '0', 'multicastIn': '0', 'bytesOut': '0', 'packetsOut': '0', 'errorsOut': '0',
+            'dropsOut': '0', 'fifoOut': '0', 'frameOut': '0', 'compressedOut': '0', 'multicastOut': '0'},
+        'wlp61s0':
+            {'bytesIn': '1202280423', 'packetsIn': '1113475', 'errorsIn': '0', 'dropsIn': '0', 'fifoIn': '0',
+            'frameIn': '0', 'compressedIn': '0', 'multicastIn': '0', 'bytesOut': '60239705', 'packetsOut':
+            '308994', 'errorsOut': '0', 'dropsOut': '0', 'fifoOut': '0', 'frameOut': '0', 'compressedOut': '0',
+            'multicastOut': '0'},
+        'docker0':
+            {'bytesIn': '0', 'packetsIn': '0', 'errorsIn': '0', 'dropsIn': '0', 'fifoIn': '0', 'frameIn': '0',
+            'compressedIn': '0', 'multicastIn': '0', 'bytesOut': '0', 'packetsOut': '0', 'errorsOut': '0',
+            'dropsOut': '0', 'fifoOut': '0', 'frameOut': '0', 'compressedOut': '0', 'multicastOut': '0'}
+        }
+        """
+
+        # read network statistics of all interfaces
+        with open("/proc/net/dev") as f:
+            data = f.read()
+
+        # remove training space, split on newline
+        data = data.strip().split("\n")
+
+        # headers for the data in the file
+        headers = ['bytesIn',  'packetsIn',  'errorsIn',  'dropsIn',  'fifoIn',  'frameIn',  'compressedIn',  'multicastIn',
+                   'bytesOut', 'packetsOut', 'errorsOut', 'dropsOut', 'fifoOut', 'frameOut', 'compressedOut', 'multicastOut']
+
+        # dictionary to store {interface_name: {stat_name: stat_value, ...}, ...}
+        ifaces_stats = {}
+
+        # loop over all the lines, skip first 2 headers
+        for line in data[3:]:
+            line = line.split() # split the lines elements
+            iface = line[0].strip(":") # extract interface name, w/o the included colon
+            ifaces_stats[iface] = dict(zip(headers, line[1:])) # add the list of statistics to this interfaces entry in the dictionary
+
+        return ifaces_stats
