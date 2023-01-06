4ee9d5f96c77f9cde83b540433f3787d4a743f14
Nefarious <c0deChieff@gmail.com>
2023-01-06 10:17:58 -0500

    changed back

--- core/monitor.py
+++ core/monitor.py
@@ -1,4 +1,4 @@
-import os, time, threading
+import os, sys, time, threading
 
 from .fx.design import *
 from .config.config import *
@@ -9,59 +9,43 @@ from .tools.connection import *
 
 from .utilities.term_control import *
 
-
 class CyberShield():
     current_interface: str
     interfaces: list
     
     def __init__(self, interface) -> None:
-        try:
-            self.sfx, self.cfg, self.os, self.hdw, self.pps = [ShieldFX(), Config(), OS(), Hardware(), Connection(interface)]
+        self.sfx, self.cfg, self.os, self.hdw, self.pps = [ShieldFX(), Config(), OS(), Hardware(), Connection(interface)]
 
-            """
-            Pull all interfaces to check if there more than one interface. if so, request user for the interface to use
-            """
-            print(chr(27) + "[2J") # Clear Screen
-            print("\033[?25l\x1b[37m", end="") # Hide Cursor
-            print(f"\x1b[8;{self.cfg.term.size[0]};{self.cfg.term.size[1]}t", end="") # Set Terminal Size
-            print("\x1b[0;0f", end="")
-            print(self.sfx.render_ui(), end="") # Set UI
+        """
+        Pull all interfaces to check if there more than one interface. if so, request user for the interface to use
+        """
+        
+        print("\033[?25l\x1b[37m", end="") # Hide Cursor
+        print(f"\x1b[8;{self.cfg.term.size[0]};{self.cfg.term.size[1]}t", end="") # Set Terminal Size
+        print("\x1b[0;0f", end="")
+        print(self.sfx.render_ui(), end="") # Set UI
 
-            self.set_info()
-            threading.Thread(target=self.pps.runPPS).start()
-            self.start_listener()
-        except KeyboardInterrupt:
-                print(chr(27) + "[2J") # Clear Screen
-                print("\033[0m", end="") # unfuck the terminal
-                print("\033[?25h", end="") # unfuck the terminal some more
-                os._exit(1)
+        self.set_info()
+        threading.Thread(target=self.pps.runPPS).start()
+        self.start_listener()
 
     def set_info(self) -> None:
         if self.cfg.os.display == True:
             if self.cfg.os.os_name_p != [0, 0]:
-                TerminalControl.placeText(self.cfg.os.os_name_p, self.cfg.os.labels_c, "OS: ", self.cfg.os.value_c, self.os.info.os_release.get("pretty_name"))
-                TerminalControl.placeText(self.cfg.os.os_version_p, self.cfg.os.labels_c, "OS Version: ", self.cfg.os.value_c, self.os.info.os_release.get("version_id"))
+                TerminalControl.placeText(self.cfg.os.os_name_p, self.cfg.os.labels_c, "OS: ", self.cfg.os.value_c, self.os.info.os_name)
+                TerminalControl.placeText(self.cfg.os.os_version_p, self.cfg.os.labels_c, "OS Version: ", self.cfg.os.value_c, self.os.info.os_version)
                 TerminalControl.placeText(self.cfg.os.os_kernel_p, self.cfg.os.labels_c, "OS Kernel: ", self.cfg.os.value_c, self.os.info.os_kernel)
 
         if self.cfg.hdw.display == True:
             if self.cfg.hdw.cpu_name_p != [0, 0]:
-                TerminalControl.placeText(self.cfg.hdw.cpu_name_p, self.cfg.hdw.labels_c, "CPU Name: ", self.cfg.hdw.value_c, self.hdw.info.cpu_info.get("cpu_name"))
-                TerminalControl.placeText(self.cfg.hdw.cpu_cores_p, self.cfg.hdw.labels_c, "CPU Cores: ", self.cfg.hdw.value_c, self.hdw.info.cpu_info.get("cpu_cores"))
-                TerminalControl.placeText(self.cfg.hdw.memory_name_p, self.cfg.hdw.labels_c, "Memory Name: ", self.cfg.hdw.value_c, self.hdw.info.mem_name)
-                TerminalControl.placeText(self.cfg.hdw.memory_capacity_p, self.cfg.hdw.labels_c, "Memory Capacity: ", self.cfg.hdw.value_c, f"{self.hdw.info.mem_info.get('mem_total')} Mb")
-                TerminalControl.placeText(self.cfg.hdw.memory_used_p, self.cfg.hdw.labels_c, "Memory Used: ", self.cfg.hdw.value_c, f"{self.hdw.info.mem_info.get('mem_used')} Mb")
-                TerminalControl.placeText(self.cfg.hdw.memory_free_p, self.cfg.hdw.labels_c, "Memory Free: ", self.cfg.hdw.value_c, f"{self.hdw.info.mem_info.get('mem_free')} Mb")
-                TerminalControl.placeText(self.cfg.hdw.hdd_name_p, self.cfg.hdw.labels_c, "HDD Name: ", self.cfg.hdw.value_c, self.hdw.info.hdd_name)
-                TerminalControl.placeText(self.cfg.hdw.hdd_capacity_p, self.cfg.hdw.labels_c, "HDD Capacity: ", self.cfg.hdw.value_c, f"{self.hdw.info.hdd_info.get('hdd_capacity')} Gb")
-                TerminalControl.placeText(self.cfg.hdw.hdd_usage_p, self.cfg.hdw.labels_c, "HDD Usage: ", self.cfg.hdw.value_c, f"{self.hdw.info.hdd_info.get('hdd_used')} Gb")
-
-                
+                TerminalControl.placeText(self.cfg.hdw.cpu_name_p, self.cfg.hdw.labels_c, "CPU Name: ", self.cfg.hdw.value_c, self.hdw.info.cpu_name)
+                TerminalControl.placeText(self.cfg.hdw.cpu_cores_p, self.cfg.hdw.labels_c, "CPU Cores: ", self.cfg.hdw.value_c, self.hdw.info.cpu_cores)
 
     def start_listener(self) -> None:
         while True:
             TerminalControl.placeTextAlt(self.cfg.conn.pps_p, "               ")
             TerminalControl.placeText(self.cfg.conn.pps_p, self.cfg.conn.labels_c, "PPS: ", self.cfg.conn.value_c, self.pps.f_pps)
             
-            TerminalControl.placeTextAlt(self.cfg.hdw.cpu_usage_p, "                  ")
+            TerminalControl.placeTextAlt(self.cfg.hdw.cpu_usage_p, "               ")
             TerminalControl.placeText(self.cfg.hdw.cpu_usage_p, self.cfg.hdw.labels_c, "CPU Usage: ", self.cfg.hdw.value_c, self.hdw.info.cpu_usage)
             time.sleep(1)
--- core/tools/os.py
+++ core/tools/os.py
@@ -1,70 +1,29 @@
-import os
-from os.path import exists
-
+import os, sys, time, platform, subprocess
 
 class OSInformation():
-  os_release: dict[str, str]
+    os_name: str
+    os_version: str
+    os_kernel: str
 
-  os_current_shell: str
-  os_shells: list[str]
-
-  os_kernel: str
+    os_shells: str
+    os_current_shell: str
 
 class OS():
-  def __init__(self):
-    self.info = OSInformation() # This is pointless
+    def __init__(self) -> None:
+        self.info = OSInformation()
 
-    self.info.os_release = self.get_os_release()
+        self.retrieveOS()
+        self.retrieveKernel()
+        self.retrieveShells()
 
-    self.info.current_shell = self.get_current_shell()
-    self.info.shells = self.get_shells()
+    def retrieveOS(self) -> None:
+        self.info.os_name = open("/etc/os-release", "r").read().split("\n")[1].replace("PRETTY_NAME=\"", "").replace("NAME=\"", "").replace("\"", "")
+        self.info.os_version = open("/etc/os-release", "r").read().split("\n")[2].replace("VERSION_ID=\"", "").replace("\"", "")
 
-    self.info.os_kernel = self.get_kernel()
+    def retrieveKernel(self) -> None:
+        self.info.os_kernel = platform.release()
 
-  def get_os_release(self) -> dict[str, str]:
-    variables = { }
-
-    if not exists("/etc/os-release"):
-      return variables
-
-    with open("/etc/os-release") as f:
-      data = f.read().split("\n")
-
-      for line in data:
-        if not "=" in line:
-          continue
-
-        x, y = line.split("=")
-
-        variables[x.lower()] = y.strip("\"\"")
-
-    return variables
-
-  def get_current_shell(self) -> str:
-    shell = os.environ.get("SHELL")
-
-    if "/bin/" in shell: # Attempt to remove the path
-      return shell.split("bin/")[1]
-
-    return shell
-
-  def get_shells(self) -> list[str]:
-    shells = [ ]
-
-    if not exists("/etc/shells"):
-      return shells
-    
-    with open("/etc/shells") as f:
-      data = f.read().split("\n")
-
-      for line in data:
-        if not line.startswith("/bin/"):
-          continue
-
-        shells.append(line[5:])
-
-    return [ *set(shells) ]
-
-  def get_kernel(self) -> str:
-    with open("/proc/version") as f:
-      return f.read().split(" ")[2:3][0]
+    def retrieveShells(self) -> None:
+        self.info.os_shells = subprocess.getoutput("cat /etc/shells").split("\n")[1:]
+        self.info.os_current_shell = subprocess.getoutput("echo $SHELL")
+        
