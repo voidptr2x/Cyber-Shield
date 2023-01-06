7bc99d480b14910e43fb8887a0f0ad1972a712d5
syyn <94141577+9xN@users.noreply.github.com>
2023-01-05 19:20:40 -0700

    add some sex

--- core/monitor.py
+++ core/monitor.py
@@ -19,7 +19,7 @@ class CyberShield():
         """
         Pull all interfaces to check if there more than one interface. if so, request user for the interface to use
         """
-        
+        print(chr(27) + "[2J") # Clear Screen
         print("\033[?25l\x1b[37m", end="") # Hide Cursor
         print(f"\x1b[8;{self.cfg.term.size[0]};{self.cfg.term.size[1]}t", end="") # Set Terminal Size
         print("\x1b[0;0f", end="")
@@ -40,6 +40,15 @@ class CyberShield():
             if self.cfg.hdw.cpu_name_p != [0, 0]:
                 TerminalControl.placeText(self.cfg.hdw.cpu_name_p, self.cfg.hdw.labels_c, "CPU Name: ", self.cfg.hdw.value_c, self.hdw.info.cpu_name)
                 TerminalControl.placeText(self.cfg.hdw.cpu_cores_p, self.cfg.hdw.labels_c, "CPU Cores: ", self.cfg.hdw.value_c, self.hdw.info.cpu_cores)
+                TerminalControl.placeText(self.cfg.hdw.memory_name_p, self.cfg.hdw.labels_c, "Memory Name: ", self.cfg.hdw.value_c, self.hdw.info.memory_name)
+                TerminalControl.placeText(self.cfg.hdw.memory_capacity_p, self.cfg.hdw.labels_c, "Memory Capacity: ", self.cfg.hdw.value_c, self.hdw.info.memory_capacity)
+                TerminalControl.placeText(self.cfg.hdw.memory_used_p, self.cfg.hdw.labels_c, "Memory Used: ", self.cfg.hdw.value_c, self.hdw.info.memory_used)
+                TerminalControl.placeText(self.cfg.hdw.memory_free_p, self.cfg.hdw.labels_c, "Memory Free: ", self.cfg.hdw.value_c, self.hdw.info.memory_free)
+                TerminalControl.placeText(self.cfg.hdw.hdd_name_p, self.cfg.hdw.labels_c, "HDD Name: ", self.cfg.hdw.value_c, self.hdw.info.hdd_name)
+                TerminalControl.placeText(self.cfg.hdw.hdd_capacity_p, self.cfg.hdw.labels_c, "HDD Capacity: ", self.cfg.hdw.value_c, self.hdw.info.hdd_capacity)
+                TerminalControl.placeText(self.cfg.hdw.hdd_usage_p, self.cfg.hdw.labels_c, "HDD Usage: ", self.cfg.hdw.value_c, self.hdw.info.hdd_usage)
+
+                
 
     def start_listener(self) -> None:
         while True:
