4f3503845c4ce967a3eb55985413e9ad61b13b7f
Nefarious <c0deChieff@gmail.com>
2023-01-10 01:57:47 -0500

    Update hardware.py

--- new/hardware.py
+++ new/hardware.py
@@ -27,19 +27,20 @@ HDD Capacity: 39GB
 HDD Used: 5GB
 HDD Free: 32GB
 """
+-
 import os, sys, time, subprocess, psutil
 
 class Hardware_Info:
         cpu_cores: int
         cpu_name: str
-        cpu_usage: str
+        cpu_usage: str   
 
         memory_type: str ## Still needed (DDR3/4)
         memory_capacity: str
         memory_used: str
         memory_free: str
 
-        """ ALL GPU INFO IS STILL NEEEDED """
+        """ ALL GPU INFO IS STILL NEEEDED """    
         gpu_name: str
         gpu_cores: str
         gpu_freq: str
@@ -55,11 +56,19 @@ class Hardware():
         info: Hardware_Info
         def __init__(self) -> None:
                 self.info = Hardware_Info()
-                self.cpu_info = open("/proc/cpuinfo", "r")
-                self.memory_info = open("/proc/meminfo", "r")
+                self.cpu_info, self.mem_info, self.hdd_info = self._retrieveInfo()
+
+        def _retrieveInfo(self) -> None:
+                cpu_info, mem_info, hdd_info = [open("/proc/cpuinfo", "r"), open("/proc/meminfo", "r"), psutil.disk_usage("/")]
+                cpu, mem = [cpu_info.read(), mem_info.read()]
+
+                cpu_info.close()
+                mem_info.close()
+
+                return [cpu, mem, hdd_info]
 
         def parseCPU(self) -> Hardware_Info:
-                for line in self.cpu_info.read().split("\n"):
+                for line in self.cpu_info.split("\n"):
                         if line.startswith("cpu cores"): self.info.cpu_cores = line.replace("cpu cores", "").replace(":", "").strip()
 
                         if line.startswith("model name"): self.info.cpu_name = line.replace("model name", "").replace(":", "").strip()
@@ -69,7 +78,8 @@ class Hardware():
                 return self.info
 
         def parseMEM(self) -> Hardware_Info:
-                for line in self.memory_info.read().split("\n"):
+                print(self.mem_info)
+                for line in self.mem_info.split("\n"):
                         if line.startswith("MemTotal:"): self.info.memory_capacity = round(int(line.replace("MemTotal:", "").replace("kB", "").strip()) / 1000000)
 
                         if line.startswith("MemFree:"): self.info.memory_free = round(int(line.replace("MemFree:" ,"").replace("kB", "").strip()) / 1000000)
@@ -79,11 +89,9 @@ class Hardware():
                 return self.info
 
         def retrieveHDD(self) -> Hardware_Info:
-                hdd_info = psutil.disk_usage("/")
-
-                self.info.hdd_capacity = round(hdd_info.total / (2**30))
-                self.info.hdd_used = round(hdd_info.used / (2**30))
-                self.info.hdd_free = round(hdd_info.free / (2**30))
+                self.info.hdd_capacity = round(self.hdd_info.total / (2**30))
+                self.info.hdd_used = round(self.hdd_info.used / (2**30))
+                self.info.hdd_free = round(self.hdd_info.free / (2**30))
 
                 return self.info
 
@@ -94,9 +102,13 @@ cpu_info = hdw.parseCPU()
 mem_info = hdw.parseMEM()
 hdd_info = hdw.retrieveHDD()
 
+"""
+To keep this updating. put the code below in a while loop with 'hdw._retrieveInfo()' in it
+"""
+
 print(f"\x1b[32mCyber Shield Hardware Module\x1b[0m")
 print(f"CPU Name: {cpu_info.cpu_name}\r\nCPU Cores: {cpu_info.cpu_cores}\r\nCPU Usage: {cpu_info.cpu_usage}")
 print(f"======================================")
 print(f"Memory Capacity: {mem_info.memory_capacity}GB\r\nMemory Free: {mem_info.memory_free}GB\r\nMemory Used: {mem_info.memory_used}GB")
-print(f"======================================")
+print(f"======================================") 
 print(f"HDD Capacity: {hdd_info.hdd_capacity}GB\r\nHDD Used: {hdd_info.hdd_used}GB\r\nHDD Free: {hdd_info.hdd_free}GB")
