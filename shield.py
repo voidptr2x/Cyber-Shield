3ebad3b7f39480feb1a365658c9a6b5e3dc37e99
skarn <118652388+skroud@users.noreply.github.com>
2023-01-16 00:50:14 -0600

    nigger

--- shield.py
+++
@@ -1,9 +0,0 @@
-import sys
-from core.monitor import *
-
-
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print(f"[ x ] Error, Invalid argument provided\r\nUsage: {sys.argv[0]} <interface>")
-    exit()
-
-CyberShield(sys.argv[1])
